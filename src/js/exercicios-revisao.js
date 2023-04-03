243997d2267af7d6317f446ef49e376976458768
LUCAS LEITE DOS REIS <lucas.leitedosreis@gmail.com>
2023-04-03 15:23:33 -0300

    update geral

--- src/js/exercicios-revisao.js
+++ src/js/exercicios-revisao.js
@@ -8,45 +8,45 @@
 //   7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
 window.UserData = {};
 function isUserData(obj) {
-  if (
-    obj &&
-    typeof obj === "object" &&
-    ("nome" in obj || "email" in obj || "cpf" in obj)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+    if (obj &&
+        typeof obj === "object" &&
+        ("nome" in obj || "email" in obj || "cpf" in obj)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 function validJson(str) {
-  try {
-    JSON.parse(str);
-  } catch (err) {
-    return false;
-  }
-  return true;
+    try {
+        JSON.parse(str);
+    }
+    catch (err) {
+        return false;
+    }
+    return true;
 }
 function loadLocalStorage() {
-  const localUserData = localStorage.getItem("UserData");
-  if (localUserData && validJson(localUserData)) {
-    const UserData = JSON.parse(localUserData);
-    if (isUserData(UserData)) {
-      Object.entries(UserData).forEach(([key, value]) => {
-        const input = document.getElementById(key);
-        if (input instanceof HTMLInputElement) {
-          input.value = value;
-          window.UserData = value;
+    const localUserData = localStorage.getItem("UserData");
+    if (localUserData && validJson(localUserData)) {
+        const UserData = JSON.parse(localUserData);
+        if (isUserData(UserData)) {
+            Object.entries(UserData).forEach(([key, value]) => {
+                const input = document.getElementById(key);
+                if (input instanceof HTMLInputElement) {
+                    input.value = value;
+                    window.UserData = value;
+                }
+            });
         }
-      });
     }
-  }
 }
 loadLocalStorage();
 function handleInput({ target }) {
-  if (target instanceof HTMLInputElement) {
-    window.UserData[target.id] = target.value;
-    localStorage.setItem("UserData", JSON.stringify(window.UserData));
-  }
+    if (target instanceof HTMLInputElement) {
+        window.UserData[target.id] = target.value;
+        localStorage.setItem("UserData", JSON.stringify(window.UserData));
+    }
 }
 const form = document.querySelector("#form");
 form?.addEventListener("keyup", handleInput);
