a48626b8e04e9e85c7d666f032deec9f41a97a94
LUCAS LEITE DOS REIS <lucas.leitedosreis@gmail.com>
2023-04-04 15:59:34 -0300

    Estudando Tuples

---
+++ src/js/Tuples.ts
@@ -0,0 +1,28 @@
+// Tuples são arrays que possuem dados em posições fixas.
+
+const produto3: (string | number)[] = ["Notebook", 2500];
+const produto4: [string, number] = ["Notebook", 2500];
+
+const nome1 = produto3[0]; // string | number
+const nome2 = produto4[0]; // string
+
+const [nome, valor] = produto4;
+
+// as const
+// Torna um dado readonly e infere o tipo de dado mais específico possível.
+// Em métodos que retornam Array's, as mesmas são transformadas em Tuples.
+
+function getText(selector: string) {
+  const el = document.querySelector<HTMLElement>(selector);
+  if (el) {
+    return [el, el.innerText] as const;
+  } else {
+    return null;
+  }
+}
+
+const button = getText("button");
+
+if (button) {
+  const [buttonElement, buttonText] = button;
+}
